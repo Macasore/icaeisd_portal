7f1febd3da79288ecbc7bf67c60fa2a56a884330
macanotmarker <macasorekingdavid@gmail.com>
2024-10-08 19:42:46 +0100

    update migrations

--- migrations1/env.py
+++ migrations1/env.py
@@ -5,6 +5,7 @@ from flask import current_app
 
 from alembic import context
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -12,8 +13,9 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
-logger = logging.getLogger('alembic.env')
 config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URI'))
+
+logger = logging.getLogger('alembic.env')
 
 
 def get_engine():
