ad25a7d053dec35171eb1d44b778d7bfc2d4c229
macanotmarker <macasorekingdavid@gmail.com>
2024-10-26 17:12:45 +0100

    feat: edit paper endpoint

--- app/authors/routes.py
+++ app/authors/routes.py
@@ -59,8 +59,8 @@ def submitPaper():
         return jsonify({"msg": "Only PDF files are allowed"}), 400
     
     file_name = secure_filename(file.filename)
-    theme = request.form.get("theme").replace(" ", "_")
-    subtheme = request.form.get("subtheme").replace(" ", "_")
+    theme = request.form.get("theme")
+    subtheme = request.form.get("subtheme")
     upload_path = f"{theme}/{subtheme}/{file_name}"
     
     try:
@@ -230,4 +230,109 @@ def deletePaper():
     
     return jsonify({"msg": "Paper deleted successfully"}), 200
     
+@author_bp.route("/edit-paper/<int:paper_id>", methods=["PUT"])
+@jwt_required()
+def editPaper(paper_id):
+    current_user = get_jwt_identity() 
+    current_user_email = User.query.filter_by(id=current_user).first().email
     
+    author = User.query.filter_by(id=current_user).first()
+    
+    if not author:
+        return jsonify({"msg": "User not found"}), 404
+    
+    if author.role != Role.AUTHOR:
+        return jsonify({"msg": "Unauthorized for this operation"}), 403
+
+    paper = Paper.query.get(paper_id)
+    if not paper:
+        return jsonify({"msg": "Paper not found"}), 404
+
+    if paper.author_id != author.id:
+        return jsonify({"msg": "You are not authorized to edit this paper"}), 403
+
+    title = request.form.get("title", paper.title)
+    abstract = request.form.get("abstract", paper.abstract)
+    coauthors = request.form.get("coauthors", []) 
+
+    is_valid, word_count = validate_abstract(abstract)
+    if not is_valid:
+        return jsonify({"msg": f"Abstract is too long. Current word count is {word_count}, but the limit is {MAX_ABSTRACT_WORDS} words."}), 400
+
+    file_path = paper.file_path
+    
+    if 'file' not in request.files:
+        return jsonify({"msg": "No file part in the request"}), 400
+    
+
+    file = request.files.get('file')
+
+    if file.filename == '':
+        return jsonify({"msg": "No selected file"}), 400
+
+    if request.content_length > MAX_FILE_SIZE:
+        return jsonify({"msg": "File size exceeds the limit of 5 MB."}), 400
+
+    if not file.filename.lower().endswith('.pdf'):
+        return jsonify({"msg": "Only PDF files are allowed"}), 400
+
+    file_name = secure_filename(file.filename)
+    theme = request.form.get("theme", paper.theme)
+    subtheme = request.form.get("subtheme", paper.subtheme)
+    upload_path = f"{theme}/{subtheme}/{file_name}"
+
+    try:
+        ftp = ftplib.FTP(current_app.config['FTP_HOST'])
+        ftp.login(current_app.config['FTP_USER'], current_app.config['FTP_PASS'])
+        ftp.cwd('/uploads')
+
+        try:
+            ftp.mkd(theme)
+        except ftplib.error_perm:
+            pass 
+
+        try:
+            ftp.mkd(f"{theme}/{subtheme}")
+        except ftplib.error_perm:
+            pass 
+        ftp.storbinary(f"STOR {upload_path}", file.stream)  
+        ftp.quit()
+
+        file_path = f"ftp://{current_app.config['FTP_HOST']}/{upload_path}"
+
+    except ftplib.all_errors as e:
+        return jsonify({"msg": f"FTP upload failed: {str(e)}"}), 500
+    
+    upload_path = "/uploads/"+ upload_path
+
+    paper.title = title
+    paper.abstract = abstract
+    paper.file_path = upload_path
+    paper.theme = theme
+    paper.subtheme = subtheme
+
+
+    CoAuthor.query.filter_by(paper_id=paper.id).delete()
+    
+    if coauthors:
+        try:
+            coauthors = json.loads(coauthors) 
+        except json.JSONDecodeError:
+            return jsonify({"msg": "Invalid coauthor data format"}), 400
+        
+        for coauthor_data in coauthors:
+            coauthor_email = coauthor_data.get('email')
+            can_submit_coauthor, message = check_paper_limits(coauthor_email)
+
+            if not can_submit_coauthor:
+                return jsonify({"msg": f"Coauthor {coauthor_email} cannot be added: {message}"}), 400
+
+            coauthor = CoAuthor(full_name=coauthor_data.get('name'), email=coauthor_email, paper=paper)
+            db.session.add(coauthor)
+
+    db.session.commit()
+    
+    message = "Dear Author,\n\nYour submission has been updated. Please check the portal for the status of your manuscript.\n\n\nRegards,\nICAEISD 2024 Team."
+    sendEmail("Paper Update", message, current_user_email)
+
+    return jsonify({"msg": "Paper updated successfully"}), 200
