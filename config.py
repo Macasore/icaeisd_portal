d487ff9b862596aabafc73441a28eacac65539ef
macanotmarker <macasorekingdavid@gmail.com>
2024-12-10 10:41:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = int(os.getenv("MAIL_PORT"))
+    MAIL_PORT = int(os.getenv("MAIL_PORT"), 587)
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') == 'True'
